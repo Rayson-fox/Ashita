5d694ff05907f084ae3d0e15736886ea9f4998f9
Rayson-fox <alexandreraynalson000@gmail.com>
2025-11-10 21:03:51 -0500

    Update SESSION_ID to use a specific value

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'Your SESSION ID Put' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID ===𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=4b8d06d9c6dbe25c,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
